5b898f3211a318ea317307e8677d8d2252db5cfc
Anshika Soni <anshikasoni357@gmail.com>
2024-12-20 03:21:02 +0530

    Java record

---
+++ Java/Main.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Anshika", 23);
+        System.out.println(person.name());
+        System.out.println(person.age());
+    }
+}
+
+record Person(String name, int age){};
+
+
+// We can also define records insdie the class such as below
+
+
+// public class Company {
+//     private final String name;
+    
+//     public Company(String name) {
+//         this.name = name;
+//     }
+
+//     // Nested record inside the Company class
+//     public record Employee(String name, int age) {}
+// }
