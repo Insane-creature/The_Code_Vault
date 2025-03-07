aa66bb547ce6254102973803c1db750c67ebf23a
Anshika Soni <anshikasoni357@gmail.com>
2025-03-07 12:33:01 +0530

    Inter prep python

---
+++ asdh.js
@@ -0,0 +1,26 @@
+// // console.log("print")
+
+// console.log(a); 
+// const a = 10;
+
+
+
+// // let: reference error
+// // const: 
+// // var: undefined
+
+function outer() {
+    let count = 0;
+    return function inner() {
+        count++;
+        console.log(count);
+    };
+}
+
+fn 
+// = outer();
+// fn(); // ?
+// fn(); // ?
+
+
+console.log(typeof fn)
