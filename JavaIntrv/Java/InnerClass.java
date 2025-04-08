ff2f0f7c8347aa0ac63a4c9a064d086bac1e6df4
Anshika Soni <anshikasoni357@gmail.com>
2025-04-08 14:55:03 +0530

    Java Adv

---
+++ JavaIntrv/Java/InnerClass.java
@@ -0,0 +1,24 @@
+package Java;
+
+class A{
+    int age;
+    public void show(){
+        System.out.println("In show");
+    }
+    class B{
+        public void config(){
+            System.out.println("In config");
+        }
+    }
+}
+
+public class InnerClass {
+    public static void main(String[] args) {
+        A obj = new A();
+        // obj.show();
+
+        A.B obj1 = obj.new B();
+        obj1.config();
+        
+    }
+}
