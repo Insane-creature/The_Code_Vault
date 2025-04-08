ab906f1b30af076452157d3efe5349792c8c3d2e
Anshika Soni <anshikasoni357@gmail.com>
2025-04-09 01:32:28 +0530

    Abstraction

--- JavaIntrv/1monthplan/Week1/Abstraction_Shape.java
+++ JavaIntrv/1monthplan/Week1/Abstraction_Shape.java
@@ -1,3 +1,23 @@
-public class Abstraction_Shape {
+abstract class Caro{
+    public abstract void drive(); 
+    public void playMusic(){
+        System.out.println("play music");
+    }
+}
+
+class childClass extends Caro{
+
+    @Override
+    public void drive() {
+        System.out.println("Implementation of abstract method");
+    }
     
+}
+
+public class Abstraction_Shape {
+    public static void main(String[] args) {
+        Caro obj = new Caro();
+        obj.drive();
+        obj.playMusic();
+    }
 }
