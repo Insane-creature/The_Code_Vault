cd3c469c20b32cdae534f889fcfd131d44276b46
Anshika Soni <anshikasoni357@gmail.com>
2025-04-11 23:43:38 +0530

    Wee1

--- JavaIntrv/1monthplan/Week1/F_LinkedList.java
+++ JavaIntrv/1monthplan/Week1/F_LinkedList.java
@@ -1,5 +1,51 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
 public class F_LinkedList {
+    
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+    // Helper to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("NULL");
+    }
+
     public static void main(String[] args) {
-        System.out.println();
+        // Create sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        ListNode reversed = reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(reversed);
     }
 }
