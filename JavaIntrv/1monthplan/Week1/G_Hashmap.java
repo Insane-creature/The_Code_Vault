4bb3c6ad5e1cb73731a071435c8c71185e4104df
Anshika Soni <anshikasoni357@gmail.com>
2025-04-13 16:10:34 +0530

    DSA 1

--- JavaIntrv/1monthplan/Week1/G_Hashmap.java
+++ JavaIntrv/1monthplan/Week1/G_Hashmap.java
@@ -1,5 +1,29 @@
+
+import java.util.HashMap;
+
 public class G_Hashmap {
-    public static void main(String[] args) {
+    public int[] TwoSum(int[] nums, int target) {
         
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+
+            if (mpp.containsKey(complement)) {
+                return new int[] { mpp.get(complement), i };
+            }
+            mpp.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,9,3,12,1};
+        int target = 11;
+
+        G_Hashmap obj = new G_Hashmap();
+        int[] result = obj.TwoSum(nums, target);
+
+        System.out.println(result[0] + ", " + result[1]);
+
     }
 }
---
+++ JavaIntrv/1monthplan/Week1/I_Multithreading.java
@@ -0,0 +1,5 @@
+public class I_Multithreading {
+    public static void main(String[] args) {
+        
+    }
+}
