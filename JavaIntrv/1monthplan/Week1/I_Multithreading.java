e82f51c3e45aa3c034f04d4f10ebcd0901df4e54
Anshika Soni <anshikasoni357@gmail.com>
2025-04-16 21:13:09 +0530

    Week 1 done

--- JavaIntrv/1monthplan/Week1/I_Multithreading.java
+++ JavaIntrv/1monthplan/Week1/I_Multithreading.java
@@ -1,5 +1,49 @@
+ class A extends Thread{
+    public void run(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hi from B");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 public class I_Multithreading {
     public static void main(String[] args) {
-        
+        A obj = new A();
+        B obj1 = new B();
+
+        // Every thread has some priority: range-> 1 to 10: 5 is mid
+        // System.out.println(obj1.getPriority());
+
+        // Setting priority to 2 means high 
+        // You can't exactly set the priority but jsut suggest it to schedular to run this on priority
+        // obj1.setPriority(2);
+        // obj1.setPriority(Thread.MAX_PRIORITY);
+
+
+        obj.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException ex) {
+        }
+        obj1.start();
+
     }
 }
--- JavaIntrv/1monthplan/Week1/J_ExceptionHandling.java
+++ JavaIntrv/1monthplan/Week1/J_ExceptionHandling.java
@@ -1,3 +1,11 @@
 public class J_ExceptionHandling {
-    
+    public static void main(String[] args) {
+        int i = 0;
+        int j = i/2;
+        try {
+            System.out.println(j);
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+    }
 }
